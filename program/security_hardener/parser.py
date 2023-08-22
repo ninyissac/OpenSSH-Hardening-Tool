70cad9fec31b287f421a5c56d767d7d4997337cf
Niny Issac <ninyissac@gmail.com>
2023-08-22 17:26:56 +0100

    trying to make csv file as the only template file for keywords and values

--- program/security_hardener/parser.py
+++ program/security_hardener/parser.py
@@ -3,7 +3,7 @@ import re
 import glob
 
 def parse_template(**kwargs):
-# Function for parsing context template files and system ssh config file
+# Function for parsing context template text files and system ssh config file
 
     parsed_lines = {}
     include_paths = []
@@ -75,4 +75,43 @@ def parse_compiler(**kwargs):
             parsed_lines.append(parsed_dict)
 
     print("In fuc pc, parsed_lines:", parsed_lines)
+    return parsed_lines
+
+def parse_csv_template_two_col(**kwargs):
+# Function for parsing context template .csv file
+    # This function is for getting the first two columns
+
+    print("In func pt_fs:")
+    parsed_lines = {}
+
+    ctx = kwargs["ctx"]
+    template_file = ctx[TEMPLATE_COMPILER_PATH]
+
+    with open(template_file, "r") as f:
+        lines = f.readlines()
+
+        for i, line in enumerate(lines):
+            line = line.strip()  # Remove leading and trailing whitespace
+
+            # Skip lines that are empty or start with '#'
+            if not line or line.startswith('#'):
+                continue
+
+            comma_value = False
+            m = re.search(r'\\,', line)
+            if m is not None:
+                l = line.replace('\\,', '!')
+                comma_value = True
+            else:
+                l = line
+            v = [x.strip() for x in l.split(",")]
+
+            if comma_value:
+                v[1] = v[1].replace('!', ',')
+
+            if i > 0:
+                keyword = v[0] # v[0] the first, v[1] the second, v[3] the third and so on.
+                value = v[1]   # here v[1] is the value to v[0] , as per the csv file structure
+                parsed_lines[keyword] = value
+
     return parsed_lines
