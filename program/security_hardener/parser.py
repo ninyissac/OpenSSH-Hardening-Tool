ddfa03fa4b46517ad6efa1a512f26eb2b4635166
Niny Issac <ninyissac@gmail.com>
2023-08-22 16:59:29 +0100

    Added code for parsing comments in csv template files

--- program/security_hardener/parser.py
+++ program/security_hardener/parser.py
@@ -2,8 +2,8 @@ from security_hardener.constants import *
 import re
 import glob
 
-# Function for parsing context template files and system ssh config file
 def parse_template(**kwargs):
+# Function for parsing context template files and system ssh config file
 
     parsed_lines = {}
     include_paths = []
@@ -37,8 +37,8 @@ def parse_template(**kwargs):
     return parsed_lines, include_paths  # Return both parsed_lines and include_paths
 
 
-# Function for parsing context template .csv file
 def parse_compiler(**kwargs):
+# Function for parsing context template .csv file
 
     parsed_lines = []
     ctx = kwargs["ctx"]
@@ -48,28 +48,31 @@ def parse_compiler(**kwargs):
     with open(template_file, "r") as f:
         lines = f.readlines()
 
-        k = []
+        k = None  # Initialize k as None
+
         for i, line in enumerate(lines):
-            
-            if not line.strip():
+            line = line.strip()
+            if not line or line.startswith('#'):  # process comments
+                print("In func pc, line for #:", line)
+                continue
+
+            if k is None: # process header
+                k = [x.strip() for x in line.split(",")]
+                print("In func pc, k:", k)
                 continue
 
             comma_value = False
             m = re.search(r'\\,', line)
             if m is not None:
-                l = line.replace('\\,', '!')
+                line = line.replace('\\,', '!')
                 comma_value = True
-            else:
-                l = line
-            v = [x.strip() for x in l.split(",")]
+            v = [x.strip() for x in line.split(",")]
 
             if comma_value:
                 v[1] = v[1].replace('!', ',')
 
-            if i == 0:
-                k = v
+            parsed_dict = dict(zip(k, v))
+            parsed_lines.append(parsed_dict)
 
-            else:
-                parsed_lines.append(dict(zip(k, v)))
-
+    print("In fuc pc, parsed_lines:", parsed_lines)
     return parsed_lines
