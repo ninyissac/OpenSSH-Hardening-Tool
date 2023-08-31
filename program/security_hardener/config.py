93e160a48b62eb71b4e51a2e806f1320955a02fd
Niny Issac <ninyissac@gmail.com>
2023-08-31 16:05:35 +0100

    recommendation change for general server, and comments added to code

--- program/security_hardener/config.py
+++ program/security_hardener/config.py
@@ -3,21 +3,21 @@ import os
 
 config = ConfigParser(interpolation=ExtendedInterpolation())
 
+# Path to the configuration file
 # CONFIG_PATH = "program/config.ini"
-
 CONFIG_PATH = os.environ.get("CONFIG_PATH")
-
 config.read(CONFIG_PATH)
 
+# Extract information about contexts and program details
 contexts_config = {}
 for k, v in config.items("contexts"):
     contexts_config[k] = v
 
 program_name = contexts_config["name"]
 program_description = contexts_config["description"]
-
 contexts = contexts_config["contexts"].split(",")
 
+# Extract context-specific settings
 context_config = {}
 for c in contexts:
     c = c.strip()
